7b9b13224116faac71c1d3bb154f853fbe9e99f9
Luciana Marques <lucianadacostamarques@gmail.com>
2018-09-29 09:46:40 -0300

    made simple stuff with cannon

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = new Vector3(0, 0, 0);
+        transform.position = pos;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            transform.position = new Vector3(1, 1, 1);
+        }
+		
+	}
+}
